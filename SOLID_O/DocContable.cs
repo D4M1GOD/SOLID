9937d71d952a8b701fae0fb44902723ecaffa89c
D4M1GOD <damian20002010@live.com>
2023-06-06 11:23:15 -0500

    Add OCP

---
+++ SOLID_O/DocContable.cs
@@ -0,0 +1,25 @@
+public class DocContable
+{
+    public DocContable(TipoDocContable tipo, int numero)
+    {
+        Tipo = tipo;
+        Numero = numero;
+    }
+    public DateTime Fecha { get; set; }
+    public double Importe { get; set; }
+    public int Numero { get; set; }
+    public TipoDocContable Tipo { get; set; }
+    public string Descripcion()
+    {
+        switch (Tipo)
+        {
+            case TipoDocContable.Factura:
+                return $"FC-{Numero}";
+            case TipoDocContable.NotaCredito:
+                return $"FC-{Numero}";
+            default: 
+                return "N/D";
+        }
+    }
+    
+}
---
+++ SOLID_O/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DocContable Fac1 = new DocContable(TipoDocContable.Factura,14312);
+        Console.WriteLine(Fac1.Descripcion());
+        Console.ReadKey();
+    }
+}
